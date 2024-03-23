3a11c1901e5c1dd17498818b8742c2148ee2a6e5
MandyJavan <javanbakhtmeh@gmail.com>
2024-03-23 15:29:26 +0330

    To check if there is any bug

--- script.js
+++ script.js
@@ -1,10 +1,22 @@
-let bookmarks = document.querySelectorAll(".bookmark")
-let items = document.querySelectorAll("issue")
-let myBody = document.body
+let bookmarks = document.querySelectorAll(".bookmark");
+let items = document.querySelectorAll(".issue");
+let myBody = document.body;
 
 for (let i = 0; i < bookmarks.length; i++) {
-    bookmarks[i].addEventListener("click", () => {
-        myBody.style.backgroundColor = bookmarks[i].getAttribute("data-color")
-    })
+  bookmarks[i].addEventListener("click", () => {
+    myBody.style.backgroundColor = bookmarks[i].getAttribute("data-color");
+  });
 }
 
+window.addEventListener("scroll", () => {
+    items.forEach(item => {
+      let rect = item.getBoundingClientRect();
+      console.log(rect)
+      let isVisible = rect.top < window.innerHeight && rect.bottom > 0;
+      if (isVisible) {
+        myBody.style.backgroundColor = item.getAttribute( "data-color" );
+      }
+    })
+});
+
+console.log(window.innerWidth)
