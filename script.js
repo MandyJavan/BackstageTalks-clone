ebcc5d8d8a10b2cb970a09ef5e0a02496adcda0b
MandyJavan <javanbakhtmeh@gmail.com>
2024-04-06 15:19:57 +0330

    ready to merge with scrolling branch

---
+++ script.js
@@ -0,0 +1,20 @@
+let bookmarks = document.querySelectorAll(".bookmark");
+let items = document.querySelectorAll(".issue");
+let myBody = document.body;
+
+for (let i = 0; i < bookmarks.length; i++) {
+  bookmarks[i].addEventListener("click", () => {
+    myBody.style.backgroundColor = bookmarks[i].getAttribute("data-color");
+  });
+}
+
+window.addEventListener("scroll", () => {
+    items.forEach(item => {
+      let rect = item.getBoundingClientRect();
+      let isVisible = (rect.top + (rect.top - (2/3))) < window.innerHeight && rect.bottom > 0;
+      if (isVisible) {
+        myBody.style.backgroundColor = item.getAttribute( "data-color" );
+      }
+    })
+});
+
